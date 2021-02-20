e72a5421c7ac90aa86d66240861b44f14dae8161
wonzrzeczny <39885183+wonrzrzeczny@users.noreply.github.com>
2021-02-20 20:44:45 +0100

    More room layouts
    * 4 pillars in the center
    * 4 pillars near the walls
    * huge hole in the middle (super glitched atm)

--- ASCII_FPS/Scenes/SceneGenerator.cs
+++ ASCII_FPS/Scenes/SceneGenerator.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ASCII_FPS.Scenes
 {
@@ -86,12 +87,11 @@ namespace ASCII_FPS.Scenes
                     Vector2 roomCenter = new Vector2((left + right) / 2, (top + bottom) / 2);
 
                     zones[x, y] = new Zone(new RectangleF(left, bottom, tileSize, tileSize));
-                    zones[x, y].AddMesh(SceneGenUtils.MakeFloor(left, right, bottom, top, 4f));
 
                     List<Triangle> wallTriangles = new List<Triangle>();
                     foreach (Vector2[] wall in walls)
                     {
-                        wallTriangles.AddRange(SceneGenUtils.MakeWall(wall, 4f, ASCII_FPS.texture1));
+                        wallTriangles.AddRange(SceneGenUtils.MakeWall(wall, -4f, 4f, ASCII_FPS.texture1));
                     }
                     MeshObject wallObject = new MeshObject(wallTriangles, new Vector3(roomCenter.X, 0f, roomCenter.Y), 0f);
                     zones[x, y].AddMesh(wallObject);
@@ -102,7 +102,12 @@ namespace ASCII_FPS.Scenes
                         scene.AddWall(wall[0] + roomCenter, wall[1] + roomCenter);
                     }
 
-                    PopulateRoom(scene, zones[x, y], x, y);
+                    PopulateRoomResults results = PopulateRoom(scene, zones[x, y], x, y);
+
+                    if (results.GenerateFloor)
+                    {
+                        zones[x, y].AddMesh(SceneGenUtils.MakeFloor(left, right, bottom, top, 4f));
+                    }
                 }
             }
 
@@ -148,13 +153,19 @@ namespace ASCII_FPS.Scenes
         }
 
 
-        private void PopulateRoom(Scene scene, Zone zone, int x, int y)
+        private PopulateRoomResults PopulateRoom(Scene scene, Zone zone, int x, int y)
         {
+            PopulateRoomResults results = new PopulateRoomResults
+            {
+                GenerateFloor = true
+            };
+
             PopulateSchemeFlags flags = new PopulateSchemeFlags
             {
                 ClearCenter = true,
                 NotJoint = true,
-                SingleEnemy = true
+                SingleEnemy = true,
+                ClearFloor = true
             };
 
             float left = x * tileSize - size * tileSize / 2;
@@ -167,6 +178,7 @@ namespace ASCII_FPS.Scenes
             {
                 flags.ClearCenter = false;
                 flags.SingleEnemy = false;
+                flags.ClearFloor = false;
 
                 MeshObject exit = new MeshObject(ASCII_FPS.exitModel, ASCII_FPS.exitTexture,
                     new Vector3(roomCenter.X, -2f, roomCenter.Z));
@@ -196,6 +208,7 @@ namespace ASCII_FPS.Scenes
                     if (rand.Next(4) == 0) // barrel
                     {
                         flags.ClearCenter = false;
+                        flags.ClearFloor = false;
 
                         int rnd = rand.Next(6);
                         Collectible.Type type = rnd < 3 ? Collectible.Type.Skill : rnd < 5 ? Collectible.Type.Armor : Collectible.Type.Health;
@@ -218,20 +231,23 @@ namespace ASCII_FPS.Scenes
 
             if (rand.Next(8) == 0 || (flags.SingleEnemy && rand.Next(2) == 0)) // special room
             {
-                PopulateSpecialRoom(scene, zone, roomCenter, flags);
+                PopulateSpecialRoom(scene, zone, roomCenter, flags, ref results);
             }
             else
             {
-                PopulateRoomCenter(scene, zone, roomCenter, flags);
-                PopulateRoomWalls(scene, zone, roomCenter, flags);
+                PopulateRoomCenter(scene, zone, roomCenter, flags, ref results);
+                PopulateRoomWalls(scene, zone, roomCenter, flags, ref results);
             }
+
+            return results;
         }
 
-        private void PopulateSpecialRoom(Scene scene, Zone zone, Vector3 roomCenter, PopulateSchemeFlags flags)
+        private void PopulateSpecialRoom(Scene scene, Zone zone, Vector3 roomCenter, PopulateSchemeFlags flags, ref PopulateRoomResults results)
         {
+            int rng = rand.Next(3);
             if (flags.SingleEnemy)
             {
-                if (rand.Next(2) == 0) // arena
+                if (rng == 0) // arena
                 {
                     Vector2[] points = new Vector2[]
                     {
@@ -252,13 +268,35 @@ namespace ASCII_FPS.Scenes
                     }
                 }
             }
-        }
-
-        private void PopulateRoomCenter(Scene scene, Zone zone, Vector3 roomCenter, PopulateSchemeFlags flags)
-        {
             if (flags.ClearCenter)
             {
-                int rnd = rand.Next(3);
+                if (rng == 1) // void
+                {
+                    results.GenerateFloor = false;
+
+                    zone.AddMesh(SceneGenUtils.MakeFloor(roomCenter.X - 50f, roomCenter.X - 30f, roomCenter.Z - 50f, roomCenter.Z + 50f, 4f));
+                    zone.AddMesh(SceneGenUtils.MakeFloor(roomCenter.X + 30f, roomCenter.X + 50f, roomCenter.Z - 50f, roomCenter.Z + 50f, 4f));
+                    zone.AddMesh(SceneGenUtils.MakeFloor(roomCenter.X - 30f, roomCenter.X + 30f, roomCenter.Z - 50f, roomCenter.Z - 30f, 4f));
+                    zone.AddMesh(SceneGenUtils.MakeFloor(roomCenter.X - 30f, roomCenter.X + 30f, roomCenter.Z + 30f, roomCenter.Z + 50f, 4f));
+
+                    Vector2[] walls = new Vector2[]
+                    {
+                        new Vector2(-30f, -30f),
+                        new Vector2(-30f, 30f),
+                        new Vector2(30f, 30f),
+                        new Vector2(30f, -30f),
+                        new Vector2(-30f, -30f)
+                    };
+                    SceneGenUtils.AddWalls(scene, zone, new List<Vector2[]> { walls }, -50f, -4f, ASCII_FPS.texture1, roomCenter);
+                }
+            }
+        }
+
+        private void PopulateRoomCenter(Scene scene, Zone zone, Vector3 roomCenter, PopulateSchemeFlags flags, ref PopulateRoomResults results)
+        {
+            int rnd = rand.Next(4);
+            if (flags.ClearCenter)
+            {
                 if (rnd == 0) // square pillar
                 {
                     Vector2[] points = new Vector2[]
@@ -288,11 +326,39 @@ namespace ASCII_FPS.Scenes
                     SceneGenUtils.AddWalls(scene, zone, new List<Vector2[]> { points }, 4f, ASCII_FPS.texture1, roomCenter);
                 }
             }
+            else
+            {
+                if (rnd == 2) // 4 pillars
+                {
+                    Vector2[] points = new Vector2[]
+                    {
+                            new Vector2(-15f, -15f),
+                            new Vector2(-10f, -15f),
+                            new Vector2(-10f, -10f),
+                            new Vector2(-15f, -10f),
+                            new Vector2(-15f, -15f)
+                    };
+
+                    List<Vector2[]> walls = new List<Vector2[]>();
+                    for (int x = 0; x < 2; x++)
+                    {
+                        for (int y = 0; y < 2; y++)
+                        {
+                            Vector2 shift = new Vector2(x, y) * 25f;
+                            Vector2[] wall = points.Select((Vector2 v) => { return v + shift; }).ToArray();
+                            walls.Add(wall);
+                        }
+                    }
+
+                    SceneGenUtils.AddWalls(scene, zone, walls, 4f, ASCII_FPS.texture1, roomCenter);
+                }
+            }
         }
 
-        private void PopulateRoomWalls(Scene scene, Zone zone, Vector3 roomCenter, PopulateSchemeFlags flags)
+        private void PopulateRoomWalls(Scene scene, Zone zone, Vector3 roomCenter, PopulateSchemeFlags flags, ref PopulateRoomResults results)
         {
-            if (flags.NotJoint && rand.Next(3) == 0) // cut corners
+            int rnd = rand.Next(4);
+            if (flags.NotJoint && rnd == 0) // cut corners
             {
                 List<Vector2[]> walls = new List<Vector2[]>
                 {
@@ -303,6 +369,30 @@ namespace ASCII_FPS.Scenes
                 };
                 SceneGenUtils.AddWalls(scene, zone, walls, 4f, ASCII_FPS.texture1, roomCenter);
             }
+            else if (rnd == 1) // 4 pillars
+            {
+                Vector2[] points = new Vector2[]
+                {
+                        new Vector2(-35f, -35f),
+                        new Vector2(-25f, -35f),
+                        new Vector2(-25f, -25f),
+                        new Vector2(-35f, -25f),
+                        new Vector2(-35f, -35f)
+                };
+
+                List<Vector2[]> walls = new List<Vector2[]>();
+                for (int x = 0; x < 2; x++)
+                {
+                    for (int y = 0; y < 2; y++)
+                    {
+                        Vector2 shift = new Vector2(x, y) * 60f;
+                        Vector2[] wall = points.Select((Vector2 v) => { return v + shift; }).ToArray();
+                        walls.Add(wall);
+                    }
+                }
+
+                SceneGenUtils.AddWalls(scene, zone, walls, 4f, ASCII_FPS.texture1, roomCenter);
+            }
         }
 
 
@@ -311,6 +401,7 @@ namespace ASCII_FPS.Scenes
             public bool ClearCenter { get; set; }
             public bool NotJoint { get; set; }
             public bool SingleEnemy { get; set; }
+            public bool ClearFloor { get; set; }
 
             public uint Mask
             {
@@ -320,6 +411,7 @@ namespace ASCII_FPS.Scenes
                     if (ClearCenter)    ret += 1;
                     if (NotJoint)       ret += 2;
                     if (SingleEnemy)    ret += 4;
+                    if (ClearFloor)     ret += 8;
                     return ret;
                 }
             }
@@ -330,6 +422,11 @@ namespace ASCII_FPS.Scenes
                 uint mask = Mask;
                 return (mask | pred.Mask) == mask;
             }
+        }
+
+        private struct PopulateRoomResults
+        {
+            public bool GenerateFloor { get; set; }
         }
     }
 }
